3613bfd88c27827ee151deb45be3cde1b13fc99a
Kyle Lin <kylejlin.us@gmail.com>
2018-05-06 17:07:33 -0700

    Get something working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate piston_window;
+
+use piston_window::{
+    PistonWindow,
+    WindowSettings,
+    PressEvent
+};
+
 fn main() {
-    println!("Hello, world!");
+    let mut window: PistonWindow = WindowSettings::new(
+        "piston-tutorial",
+        [960, 720]
+    )
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    while let Some(e) = window.next() {
+        if let Some(button) = e.press_args() {
+
+        }
+    }
 }
