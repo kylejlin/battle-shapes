4c5daf88b6f7dfbcc7b706947567922bb80c102a
Kyle Lin <kylejlin.us@gmail.com>
2018-05-08 18:58:54 -0700

    Change IRON color

--- src/battle_shapes/colors.rs
+++ src/battle_shapes/colors.rs
@@ -1,5 +1,5 @@
 pub const GRASS: [f32; 4] = [0.0, 0.5, 0.0, 1.0];
-pub const IRON: [f32; 4] = [0.3, 0.3, 0.3, 1.0];
+pub const IRON: [f32; 4] = [0.5, 0.6, 0.7, 1.0];
 pub const WOOD: [f32; 4] = [0.3, 0.1, 0.1, 1.0];
 pub const STONE: [f32; 4] = [0.3, 0.3, 0.3, 1.0];
 pub const HEALTH_BAR: [f32; 4] = [0.8, 0.8, 0.0, 1.0];
